26cede5c92c85822c552d0f967c08e410692df95
Hierzi <lea.h@live.at>
2018-06-22 15:15:46 +0200

    added posts

--- app/router.js
+++ app/router.js
@@ -14,11 +14,8 @@ Router.map(function() {
     this.route('success');
     this.route('failed');
   });
-<<<<<<< HEAD
   this.route('imprint');
-=======
   this.route('nimda');
->>>>>>> fb5b323aa5f634c812e92dcfbef60f8c36d945b7
 });
 
 export default Router;
